d7e977dc05ef4d9c6ceab0cd5275638bfdb59942
Akhilesh Dhuri <40741480+akhilesh297@users.noreply.github.com>
2020-02-14 12:15:51 +0530

    14 feb

---
+++ Javascript/js/main.js
@@ -0,0 +1,17 @@
+// var a = "hello"
+// console.log(typeof (a))
+// a = 23
+// console.log(typeof (a))
+// a = true
+// console.log(typeof (a))
+// var c = 23
+// var c = 34
+// let b = "HEllo"
+// let b = "hi"
+// console.log(b)
+// const a = 23
+let a = "World"
+console.log(a.length)
+console.log(a.substring(0, 4))
+console.log(a.substr(0, 1))
+console.log(a.slice(-3, -1))
